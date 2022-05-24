2debdbc2ed1bcfd3e53b409c9fbc03f5f77aca79
ProfessorChaos <paul.lintilhac@gmail.com>
2022-05-23 22:41:25 -0400

    refactored a bunch of stuff. added more coumns to tables, fixed bug in voume calculation and my bar backfill algorithm, among others

--- setup-postgres.sh
+++ setup-postgres.sh
@@ -1,32 +1,43 @@
 #!/bin/bash 
 
 psql -c "CREATE DATABASE ftxtest"
-psql -d ftxtest -c "CREATE TABLE mixed_15_sec ( \
+psql -d ftxtest -c "CREATE TABLE mixed ( \
 	startTime timestamp NOT NULL,\
 	time BIGINT UNIQUE NOT NULL, \
 	open DECIMAL (32,8) NOT NULL, \
 	close DECIMAL ( 32,8 )  NOT NULL, \
 	high DECIMAL (32,8) NOT NULL, \
     low DECIMAL (32,8) NOT NULL, \
-    volume DECIMAL (32,8) NOT NULL \
+    volume DECIMAL (32,8) NOT NULL, \
+	pair VARCHAR(16) NOT NULL, \
+	exchange VARCHAR(16) NOT NULL, \
+	res_secs BIGINT NOT NULL, \
+	is_streamed BIT NOT NULL \
     );"
 
-psql -d ftxtest -c "CREATE TABLE historical_15_sec ( \
+psql -d ftxtest -c "CREATE TABLE hist ( \
 	startTime timestamp NOT NULL,\
 	time BIGINT UNIQUE NOT NULL, \
 	open DECIMAL (32,8) NOT NULL, \
 	close DECIMAL ( 32,8 )  NOT NULL, \
 	high DECIMAL (32,8) NOT NULL, \
     low DECIMAL (32,8) NOT NULL, \
-    volume DECIMAL (32,8) NOT NULL \
+    volume DECIMAL (32,8) NOT NULL, \
+	pair VARCHAR(16) NOT NULL, \
+	exchange VARCHAR(16) NOT NULL, \
+	res_secs BIGINT NOT NULL,
+	is_streamed BIT NOT NULL \
     );"
 
-psql -d ftxtest -c "CREATE TABLE stream_15_sec ( \
+psql -d ftxtest -c "CREATE TABLE diff ( \
 	startTime timestamp NOT NULL,\
 	time BIGINT UNIQUE NOT NULL, \
 	open DECIMAL (32,8) NOT NULL, \
 	close DECIMAL ( 32,8 )  NOT NULL, \
 	high DECIMAL (32,8) NOT NULL, \
     low DECIMAL (32,8) NOT NULL, \
-    volume DECIMAL (32,8) NOT NULL \
+    volume DECIMAL (32,8) NOT NULL, \
+	pair VARCHAR(16) NOT NULL, \
+	exchange VARCHAR(16) NOT NULL, \
+	res_secs BIGINT NOT NULL \
     );"
